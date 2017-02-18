5d58c2010a5166e0b28c9f0905df6209f2977590
Sean <seannevintsy@gmail.com>
2017-02-18 17:48:08 +0800

    Fix index.php require statement.

--- index.php
+++ index.php
@@ -1,20 +1,9 @@
-<!DOCTYPE html>
 <?php include 'header.php'; ?>
-<html>
 
-    <head>
-        
-        <link rel="stylesheet" type="text/css" href="style.css">
-        
-        <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
-    </head>
-
-    <body>  
         <img class="b0ss" src="b0ss.png" width=100%/>
         
         <div class="bottom">
-            
-            
+                       
         </div>
     </body>
 
