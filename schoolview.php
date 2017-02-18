17122e71f72ef9e2de7fb2e9ed579ccb06881d82
Tup <Tup>
2017-02-18 21:36:07 +0800

    school biew

--- schoolview.php
+++ schoolview.php
@@ -1,19 +1,24 @@
 <?php require('header.php'); ?>
 
         <div class="schoolcontainer">
-            <div>
-                <img src="b0ss.png" width="100px">
+            <div id="schoolheader">
             </div>
             <div>
                 <h1 id="school_name">ADMU de Manila</h1>
             </div>
             <div class="principal_row">
-                <p>Principal:</p>
-                <p id="principal_name">Prince C. Pal</p>
+                <p class="principal_name">Principal:</p>
+                <p class="principal_name" style="padding-left:10px">Prince C. Pal</p>
             </div>
 
+            <div style="display:flex; flex-direction:column; text-align: center">
+                
+                <p style="font-size:200%;">About the school:</p>
+                <p style="margin:auto;">GSEC BRO? GAGS PARE TUNG NUH LULH. FLY HIGH IN A FIGHT FIGHT BLUE AND WHITE</p>
+            </div>
+            
             <div>
-                <p class="principal_row">Projects</p>
+                <p style="font-size:200%;">Projects</p>
                 <ul>
                     <li>1</li>
                     <li>2</li>
