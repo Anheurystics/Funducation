08881867f00231ee52e3abb68fa06a6899d44897
Willard <willard.torres1008@gmail.com>
2017-02-18 22:28:26 +0800

    Redirect to index from dashboard if not logged in

--- dashboard.php
+++ dashboard.php
@@ -1,4 +1,8 @@
 <?php
 include 'header.php';
+if($_SESSION['logged_in'] == 0)
+{
+    echo '<meta http-equiv="refresh" content="0;index.php">';
+}
 include 'dashboard_'.$_SESSION['role'].'.php';
 ?>
