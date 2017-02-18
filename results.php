701c6b1b8a407e8c0cb929b241028bfd32e28d22
Sean <seannevintsy@gmail.com>
2017-02-18 17:56:06 +0800

    Add results page. Fix style.css

---
+++ results.php
@@ -0,0 +1,31 @@
+<?php require('header.php');?>
+
+		<div>
+			<h1>Projects</h1>
+		</div>
+		<div class="results">
+			<div class="result">
+				<div class="result-img">
+					<img src="pic.jpg"/>
+				</div>
+				<div class="result-text">
+					<div class="title">
+						<h2><a href="#">Mr. Smith's Science Classroom</a></h2>
+					</div>
+					<div class="description">
+						<p>Lorem ipsum dolor sit amet, consectetur 
+						adipiscing elit, sed do eiusmod tempor incididunt 
+						ut labore et dolore magna aliqua. Ut enim ad minim 
+						veniam, quis nostrud exercitation ullamco laboris 
+						nisi ut aliquip ex ea commodo consequat.</p>
+					</div>
+				</div>		
+				<div class="donation-stats">
+					<div class="progress-bar"></div>
+					<p>P200 still needed</p>
+				</div>
+			</div>
+		</div>
+	</body>
+
+</html>
