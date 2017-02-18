566676ef8170107d24f778a16b5e350c335284a8
lumisce <sinehye@gmail.com>
2017-02-18 23:55:54 +0800

    Add donated amount to project

--- donateview.php
+++ donateview.php
@@ -1,13 +1,11 @@
 <?php require('header.php');
-if(empty($_SESSION['logged_in']) || $_SESSION['logged_in'] == 0)
-{
+if (empty($_SESSION['logged_in']) || $_SESSION['logged_in'] == 0) {
     $_SESSION['login_redirect'] = "donateview.php?id=".$_GET['id'];
     header("Location: index.php");
     exit();
 }
 
-if($_SESSION['role'] != 'donors')
-{
+if ($_SESSION['role'] != 'donors') {
     header("Location: projectview.php?id=".$_GET['id']);
     exit();
 }
@@ -15,6 +13,16 @@ if($_SESSION['role'] != 'donors')
 if (isset($_GET['id'])) {
     $query = "select * from projects where id=" . $_GET['id'];
     $project = mysqli_fetch_assoc(mysqli_query($conn, $query));
+
+    if (isset($_POST['submit'])) {
+        $query = "update projects set collectedAmount=(collectedAmount+" . $_POST['amount'] . ") where id=" . $_GET['id'];
+        mysqli_query($conn, $query);
+        $result=mysqli_affected_rows($conn);
+        if ($result) {
+            header("Location: projectview.php?id=".$_GET['id']);
+            exit();
+        }
+    }
 } else {
     header("Location: index.php");
     exit();
@@ -26,19 +34,18 @@ if (isset($_GET['id'])) {
         <p id="donate">Donating to </p>
         <a href="./projectview.php?id=<?php echo $project["id"] ?>" title=""><p id="donate" style="padding-left:10px"><?php echo $project["name"] ?></p></a></div>
     </div>
-    <form>
+    <form action="" method="post">
         <div style="display:flex; align-items:center; flex-direction:column">
             <div>
                 <p style="text-align:center">Amount</p>
-                <textarea id="amount"></textarea>
+                <input type="number" name="amount">
             </div>
             <div>
                 <p>Mode of Payment</p>
                 <input type="radio" value"mode1"/>Bruh<br>
                 <input type="radio" value"mode2"/>Suh<br>
             </div>
-            <a href="#"><div id="submit_button">Submit</div></a>
-            <a href="#"><div id="submit_button">Back</div></a>
+            <input id="submit_button" type="submit" name="submit" value="Donate">
         </div>
     </form>
 </div>
