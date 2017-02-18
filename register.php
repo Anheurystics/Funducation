902469c511581baf894ddc09bfa3649e5e1a9bb0
Willard <willard.torres1008@gmail.com>
2017-02-18 17:27:21 +0800

    Minor file changes

--- register.php
+++ register.php
@@ -1,38 +1,35 @@
 <!DOCTYPE html>
 
 <html>
-
     <body>
-    
         <div>
             <p>Sign Up</p>
         </div>
-        
         <div>
-            <div>
-                <p>Username</p>
-                <textarea></textarea>
-            </div>
-            <div>
-                <p>Email</p>
-                <textarea></textarea>
-            </div>
-            <div>
-                <p>Password</p>
-                <textarea></textarea>
-            </div>
-            <div>
-                <p>Registering as:</p>
-                <input type="radio" value="Teacher"> Other<br>
-                <input type="radio" value="Teacher"> Teacher<br>
-                <input type="radio" value="Teacher"> Principal<br>
+            <form action="">
+                <div>
+                    <p>Username</p>
+                    <textarea></textarea>
+                </div>
+                <div>
+                    <p>Email</p>
+                    <textarea></textarea>
+                </div>
+                <div>
+                    <p>Password</p>
+                    <textarea></textarea>
+                </div>
+                <div>
+                    <p>Registering as:</p>
+                    <input type="radio" name="role" value="0">Donor<br>
+                    <input type="radio" name="role" value="1">Teacher<br>
+                    <input type="radio" name="role" value="2">Principal<br> 
+                </div>
                 
-            </div>
-            
-            <div>
-                <button>Register</button>
-            </div>
+                <div>
+                    <button>Register</button>
+                </div>
+            </form>
         </div>
     </body>
-    
 </html>
