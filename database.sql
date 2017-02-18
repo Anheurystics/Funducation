0cd87a76610d629ae25f3dd1d74f855c9f6dad41
Willard <willard.torres1008@gmail.com>
2017-02-18 16:33:50 +0800

    Initial commit

---
+++ database.sql
@@ -0,0 +1,51 @@
+drop database bh2017;
+create database bh2017;
+use bh2017;
+
+create table users (
+    id int not null primary key,
+    name varchar(255),
+    email varchar(255),
+    password varchar(255),
+    role_id int
+);
+
+create table schools (
+    id int not null primary key,
+    name varchar(255),
+    location varchar(255),
+    user_id int,
+    foreign key (user_id) references users(id)
+);
+
+create table projects (
+    id int not null primary key,
+    name varchar(255),
+    school_id int,
+    collectedAmount decimal,
+    foreign key (school_id) references schools(id)
+);
+
+create table project_teacher (
+    id int not null primary key,
+    project_id int,
+    teacher_id int,
+    foreign key (project_id) references projects(id),
+    foreign key (teacher_id) references users(id)
+);
+
+create table project_donor (
+    id int not null primary key,
+    project_id int,
+    donor_id int,
+    foreign key (project_id) references projects(id),
+    foreign key (donor_id) references users(id)
+);
+
+create table school_teacher (
+    id int not null primary key,
+    school_id int,
+    teacher_id int,
+    foreign key (school_id) references schools(id),
+    foreign key (teacher_id) references users(id)
+);
