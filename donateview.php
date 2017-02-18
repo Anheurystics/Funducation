8dfe243da8be9bd8a8bc251a306fb75c7141abcd
Tup <Tup>
2017-02-18 18:14:50 +0800

    donateview

---
+++ donateview.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html>
+
+    <body>
+    
+        <div>
+            <p>Donate</p>
+        </div>
+        
+        <form>
+
+            <div>
+                <p>Amount</p>
+                <textarea id="amount"></textarea>
+            </div>
+            <div>
+                <p>Mode of Payment</p>
+                <input type="radio" value"mode1"/>Bruh<br>
+                <input type="radio" value"mode2"/>Suh<br>
+            </div>
+            <button>Submit</button>
+        </form>
+    </body>
+    
+</html>
