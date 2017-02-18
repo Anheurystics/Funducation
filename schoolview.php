50d9ddcce0667fe067220b462bdaea5585f1d0f5
Tup <Tup>
2017-02-18 17:57:16 +0800

    schoolview.php added

---
+++ schoolview.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html>
+
+    <body>
+    
+        <div>
+            <p>Project</p>
+        </div>
+        
+        <div>
+            <img src="b0ss.png" width="100px">
+        </div>
+        <div>
+            <p id="project_name">Project Name</p>
+        </div>
+        <div>
+            <p id="school">School:</p>
+        </div>
+        <div>
+            <p>Project Description</p>
+            <p id="project_desc">WE NEED MONEY</p>
+        </div>
+        <button>Donate</button>
+    </body>
+    
+</html>
