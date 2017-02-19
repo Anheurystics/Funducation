1be43480d9a3491133e71522a65309669fb90ad6
Tup <Tup>
2017-02-19 10:44:23 +0800

    schoolview update

--- schoolview.php
+++ schoolview.php
@@ -14,59 +14,74 @@ if (isset($_GET['id'])) {
 }
 ?>
 
-<div class="schoolcontainer">
-    <div id="schoolheader">
-        <img src="b0ss.png" />
-    </div>
-    <div class="school-info">
-        <div>
-            <h1 id="school_name"><?php echo $school['name'] ?></h1>
-        </div>
-        <div class="principal_row">
-            <p>Location:</p>
-            <p id="location_name" style="padding-left:10px"><?php echo $school['location'] ?></p>
-        </div>
-        <div class="principal_row">
-            <p class="principal_name">Principal:</p>
-            <p id="principal_name" style="padding-left:10px"><?php echo $principal['name'] ?></p>
-        </div>
-    </div>
+<div style="display:flex; flex-direction:row; padding:40px" class="schoolcontainer">
+    <div style="flex:2" class="school-info">
+    
 
-    <?php
-    // if there are projects, loop through and format
-    if ($get_projects) { ?>
-    <div>
-        <h1>Projects</h1>
-        <div class="results">
-        <?php while($project = mysqli_fetch_array($get_projects)) {?>
-            <div class="result">
-                <div class="result-img">
-                    <img src="pic.jpg"/>
-                </div>
-                <div class="result-text">
-                    <div class="title">
-                        <h2><a href="./projectview.php?id=<?php echo $project['id'] ?>"><?php echo $project["name"] ?></a></h2>
+        <div style="padding:20px; border-bottom:2px solid lightgray">
+            <div>
+                <h1 id="school_name"><?php echo $school['name'] ?></h1>
+            </div>
+            <div class="principal_row">
+                <p>Location:</p>
+                <p id="location_name" style="padding-left:10px"><?php echo $school['location'] ?></p>
+            </div>
+            <div class="principal_row">
+                <p class="principal_name">Principal:</p>
+                <p id="principal_name" style="padding-left:10px"><?php echo $principal['name'] ?></p>
+            </div>
+        </div>
+
+        <?php
+        // if there are projects, loop through and format
+        if ($get_projects) { ?>
+        <div style="padding:20px;">
+            <h1 style="margin-top:0px;">Projects</h1>
+            <div class="results">
+            <?php while($project = mysqli_fetch_array($get_projects)) {?>
+                <div class="result">
+                    <div class="result-img">
+                        <img src="pic.jpg"/>
                     </div>
-                    <div class="description">
-                        <p><?php echo $project["description"] ?></p>
+                    <div class="result-text">
+                        <div class="title">
+                            <h2><a href="./projectview.php?id=<?php echo $project['id'] ?>"><?php echo $project["name"] ?></a></h2>
+                        </div>
+                        <div class="description">
+                            <p><?php echo $project["description"] ?></p>
+                        </div>
+                    </div>      
+                    <div class="donation-stats">
+                        <p>Goal: <?php echo "₱" . $project['goalAmount'] ?></p>
+                        <?php if($project['collectedAmount'] == $project['goalAmount']) { ?>
+                                <p>Accomplished</p>
+                        <?php } else { ?>
+                            <?php $percentage = (($project['collectedAmount'] * 1.0) / $project['goalAmount']) * 100;
+                            $percentage = round($percentage, 2)?>
+                            <div class="progress-bar"><div class="progress-bar-fill" style="width:<?php echo $percentage . '%';?>"></div></div>
+                            <p><?php echo "₱" . ($project['goalAmount'] - $project['collectedAmount']) ?> still needed</p>
+                        <?php } ?>
                     </div>
-                </div>      
-                <div class="donation-stats">
-                    <p>Goal: <?php echo "₱" . $project['goalAmount'] ?></p>
-                    <?php if($project['collectedAmount'] == $project['goalAmount']) { ?>
-                            <p>Accomplished</p>
-                    <?php } else { ?>
-                        <?php $percentage = (($project['collectedAmount'] * 1.0) / $project['goalAmount']) * 100;
-                        $percentage = round($percentage, 2)?>
-                        <div class="progress-bar"><div class="progress-bar-fill" style="width:<?php echo $percentage . '%';?>"></div></div>
-                        <p><?php echo "₱" . ($project['goalAmount'] - $project['collectedAmount']) ?> still needed</p>
-                    <?php } ?>
                 </div>
+            <?php } ?>
             </div>
         <?php } ?>
         </div>
-    <?php } ?>
+        
     </div>
+    
+    <div style="flex:1">
+        <div id="schoolheader">
+            <img src="b0ss.png" />
+        </div>
+
+        <div style="margin-left:20px; margin-top:10px; padding: 0px 20px 20px 20px;" class="school-info">
+            <h1>Donate Materials</h1>
+            <p style="font-size:150%;">Choose file:</p>
+            <div id="submit_button">Donate</div>
+        </div>
+    </div>
+    
 </div>
 
 <?php require('footer.php'); ?>
